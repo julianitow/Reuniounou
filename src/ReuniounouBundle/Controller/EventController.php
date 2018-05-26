6a5a2ac0b7d24840a8f862ad076ca54102ddaa49
Ander Arrosteguy <Nekr0w@users.noreply.github.com>
2018-05-26 03:35:56 +0200

    Ajout de la création d'evenements

---
+++ src/ReuniounouBundle/Controller/EventController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace ReuniounouBundle\Controller;
+
+use ReuniounouBundle\Entity\Evenement;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+
+class EventController extends Controller
+{
+    /**
+     * @Route("/event/create", name="create_event")
+     */
+    public function createAction(Request $request) {
+        $error = null;
+        $event = new Evenement();
+        $session = $request->getSession();
+        $id = $session->get('id');
+        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $event);
+
+        $formBuilder
+            ->add('titre', TextType::class, ['label'=> 'Titre', 'attr' => ['class' => "form-control", 'placeholder' => "Titre"]])
+            ->add('description', TextareaType::class, ['label'=> 'Description', 'attr' => ['class' => "form-control"]])
+            ->add('date', DateTimeType::class, ['label' => 'Date', 'attr' => ['class' => 'form-control']])
+            ->add('adresse', TextType::class, ['label'=> 'Adresse', 'attr' => ['class' => "form-control", 'placeholder' => "Adresse"]])
+            ->add('ville', TextType::class, ['label'=> 'Ville', 'attr' => ['class' => "form-control", 'placeholder' => "Ville"]])
+            ->add('Ajouter l\'événement', SubmitType::class, ['attr' => ['class'=> 'btn btn-primary']]);
+        $form = $formBuilder->getForm();
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $event = $form->getData();
+            $manager = $this->getDoctrine()->getManager();
+            $repositoryUsers = $manager->getRepository('ReuniounouBundle:Utilisateur');
+            $user = $repositoryUsers->findOneById($id);
+            $event->setUtilisateur($user);
+            $event->setTokenInvitation(base64_encode(random_bytes(10)));
+            $manager->persist($event);
+
+            try
+            {
+                $manager->flush();
+            }
+            catch (PDOException $e)
+            {
+                $error = "UniqueConstraintViolationException";
+            }
+            catch (UniqueConstraintViolationException $e)
+            {
+                $error = "UniqueConstraintViolationException";
+            }
+        }
+        return $this->render('@Reuniounou/Event/create.html.twig', ['form'=> $form->createView(), 'error'=> $error]);
+    }
+
+    /**
+     * @Route("/event/{}", name="show_event")
+     */
+    public function showAction($token) {
+
+    }
+}
---
+++ src/ReuniounouBundle/Tests/Controller/EventControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ReuniounouBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class EventControllerTest extends WebTestCase
+{
+}
